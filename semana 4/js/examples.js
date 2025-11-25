ac52bb7454dbc7807b7295c2fe97011f5402bde4
jhandrydavid3-netizen <jhandrydavid3@gmail.com>
2025-11-25 12:06:56 -0500

    CRUD de gatos con editar y eliminar

---
+++ semana 4/js/examples.js
@@ -0,0 +1,64 @@
+var vartype = "Hello";
+vartype=45;
+let vartype2 = "Hi";
+const consType = "by";
+
+
+const arrayType= ["gato", "gatitu", "galete"];
+
+const objectType= {nombre: "David", apellido: "Perez", edad: 22};
+console.boolType= false;
+const arrayOfObjects= [
+    {nombre: "David", apellido: "Perez", edad: 22},
+    {nombre: "Ana", apellido: "Lopez", edad: 25},
+];
+
+console.log();
+
+if (boolType==true){
+    console.log("BOOL TYPE= ES FALSE");
+}else{
+    console.log("BOOL TYPE= ES TRUE");  
+}
+arrayOfObjects.forEach((item)=>{
+    console.log(item.nombre);
+}   );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
